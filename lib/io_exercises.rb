e9d193902c7480911b07d506ffb26861ade6776d
Zuhair A. Shaikh <zuhair@Zuhairs-MacBook-Air.local>
2017-05-12 16:52:01 -0700

    final commit

--- lib/io_exercises.rb
+++ lib/io_exercises.rb
@@ -10,3 +10,40 @@
 #   shuffles the lines, and saves it to the file "{input_name}-shuffled.txt". You
 #   could create a random number using the Random class, or you could use the
 #   `shuffle` method in array.
+
+def guessing_game
+  rng = Random.new
+  guesses = []
+  random_num = rng.rand(1..100)
+  puts "Thank you for playing the Guessing Game"
+  loop do
+    print "\nPlease guess a number ranging from 1 to 100: "
+    guess = gets.chomp.to_i
+    guesses << guess
+    if guess == random_num
+      break
+    elsif guess > random_num
+      puts "\nYour guess: #{guess} is too high."
+    elsif guess < random_num
+      puts "\nYour guess: #{guess} is too low."
+    end
+    puts "Your guesses so far: #{guesses.join(', ')}"
+  end
+  puts "\nCongrats! You guessed the right number: #{guesses.last}"
+  puts "Your guesses: #{guesses.join(', ')}"
+  puts "You guessed a total of #{guesses.length} times."
+end
+
+def file_shuffler
+  print "Please enter the name of the file you want shuffled: "
+  file_name = gets.chomp
+  shuffled_file_array = File.readlines("#{file_name}.txt").shuffle
+  File.open("#{file_name}-shuffled.txt", "w") do |f|
+    shuffled_file_array.each { |line| f.puts line }
+  end
+end
+
+if __FILE__ == $PROGRAM_NAME
+  # guessing_game
+  # file_shuffler
+end
